6661b3b59920fd7abde423671b28d13c97ae54e9
Thomas <165504507+Thomas5109@users.noreply.github.com>
2025-10-05 20:51:17 -0400

    Pagina de busca do Cliente pelo Cpf e cadastro Pet
    foi criador a front da pagina de busca do Cliente pelo Cpf, mas ainda falta criar a pagina do novo pet

--- clinic_vet/core/urls.py
+++ clinic_vet/core/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('animais/', views.lista_animais, name='lista_animais'),#acesso da Recepcionista
-    path('agendar_consulta/', views.agendar_consulta, name='agendar_consulta'), #acesso do ADM(EDIT -- Vai na parte da views)
+    path('agendar_consulta/', views.agendar_consulta, name='agendar_consulta'),
     path('consultas/', views.lista_consultas, name='lista_consultas'),#acesso da Recepcionista
 ]
